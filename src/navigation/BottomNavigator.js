84b9fc3dc483db79186062c984f9e46e2a0be0aa
kamil-matula <43967269+kamil-matula@users.noreply.github.com>
2021-10-03 17:28:24 +0200

    Replace icons in Bottom Navigation Bar

--- src/navigation/BottomNavigator.js
+++ src/navigation/BottomNavigator.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { useTheme } from 'react-native-paper';
 import { Menu, Fridges, ShoppingLists } from '../screens';
+import { Image } from 'react-native';
+
+import menuIcon from '../../assets/images/menu.png'; 
+import fridgeIcon from '../../assets/images/fridge.png'; 
+import listIcon from '../../assets/images/list.png'; 
 
 const Tab = createMaterialBottomTabNavigator();
 const BottomNavigator = () => {
@@ -19,17 +24,47 @@ const BottomNavigator = () => {
       <Tab.Screen
         name='Menu'
         component={Menu}
-        options={{ tabBarIcon: 'widgets', tabBarLabel: 'MENU' }}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={menuIcon}
+              style={{
+                width: 20,
+                height: 20,
+                tintColor: focused ? colors.cyberYellow : colors.silverMetallic,
+              }}
+            />
+          ),
+          tabBarLabel: 'MENU',
+        }}
       />
       <Tab.Screen
         name='Fridges'
         component={Fridges}
-        options={{ tabBarIcon: 'fridge', tabBarLabel: 'FRIDGES' }}
+        options={{ tabBarIcon: ({ focused }) => (
+          <Image
+            source={fridgeIcon}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: focused ? colors.cyberYellow : colors.silverMetallic,
+            }}
+          />
+        ), tabBarLabel: 'FRIDGES' }}
       />
       <Tab.Screen
         name='Shopping Lists'
         component={ShoppingLists}
-        options={{ tabBarIcon: 'basket', tabBarLabel: 'SHOPPING LISTS' }}
+        options={{ tabBarIcon: ({ focused }) => (
+          <Image
+            source={listIcon}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: focused ? colors.cyberYellow : colors.silverMetallic,
+            }}
+          />
+        ), tabBarLabel: 'SHOPPING LISTS' }}
       />
     </Tab.Navigator>
   );
