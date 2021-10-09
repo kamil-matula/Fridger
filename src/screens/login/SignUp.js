6a8ecabeb1337408b46e17a152b2c10c60ec31c6
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-10-09 20:42:31 +0200

    Add sign up screen

---
+++ src/screens/login/SignUp.js
@@ -0,0 +1,107 @@
+import React, { useState, useRef } from 'react';
+
+import { ScrollView, Text, View } from 'react-native';
+
+import { InputField, Button } from '../../components';
+import { makeStyles } from '../../utils/makeStyles';
+
+const SignUp = () => {
+  const styles = useStyles();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [nick, setNick] = useState('');
+
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const nickRef = useRef();
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.header}>Sign Up</Text>
+      <InputField
+        label='Email'
+        textInputProps={{
+          onChangeText: setEmail,
+          value: email,
+          returnKeyType: 'next',
+          placeholder: 'Enter your email',
+          onSubmitEditing: () => passwordRef?.current?.focus(),
+          autoComplete: 'email',
+          keyboardType: 'email-address',
+        }}
+      />
+      <View style={styles.separator16} />
+      <InputField
+        label='Password'
+        textInputProps={{
+          onChangeText: setPassword,
+          value: password,
+          returnKeyType: 'next',
+          placeholder: 'Enter your password',
+          onSubmitEditing: () => confirmPasswordRef?.current?.focus(),
+          ref: passwordRef,
+        }}
+        secure={true}
+      />
+      <View style={styles.separator16} />
+      <InputField
+        label='Confirm password'
+        textInputProps={{
+          onChangeText: setConfirmPassword,
+          value: confirmPassword,
+          returnKeyType: 'next',
+          placeholder: 'Confirm your password',
+          onSubmitEditing: () => nickRef?.current?.focus(),
+          ref: confirmPasswordRef,
+        }}
+        secure={true}
+      />
+      <View style={styles.separator16} />
+      <InputField
+        label='Nick'
+        textInputProps={{
+          onChangeText: setNick,
+          value: nick,
+          returnKeyType: 'done',
+          placeholder: 'Enter your nick',
+          ref: nickRef,
+        }}
+      />
+      <View style={styles.separator40} />
+      <Button label='Sign up' variant='contained' />
+      <Text style={styles.text}>Already have an account?</Text>
+      <Button label='Login' variant='outlined' />
+      <View style={styles.separator16} />
+    </ScrollView>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: theme.colors.background,
+  },
+  header: {
+    marginVertical: 64,
+    fontSize: 36,
+    color: theme.colors.text,
+    textAlign: 'center',
+  },
+  text: {
+    marginVertical: 16,
+    fontSize: 14,
+    color: theme.colors.text,
+    textAlign: 'center',
+  },
+  separator16: {
+    marginVertical: 8,
+  },
+  separator40: {
+    marginVertical: 20,
+  },
+}));
+
+export default SignUp;
