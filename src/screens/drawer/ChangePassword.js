b016e7059d23e30ea0d16ae4c302f664ffd384a1
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-10-16 15:16:55 +0200

    Add screen to change password

--- src/screens/drawer/ChangePassword.js
+++ src/screens/drawer/ChangePassword.js
@@ -1,37 +1,103 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, View } from 'react-native';
+import { Appbar, useTheme } from 'react-native-paper';
 
-import { makeStyles } from '../../utils';
-import { AppBar } from '../../components';
+import { InputField, Button } from '../../components';
+import { makeStyles } from '../../utils/makeStyles';
 
-const ChangePassword = () => {
+const ChangePassword = ({ navigation }) => {
   const styles = useStyles();
+  const theme = useTheme();
+
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  const newPasswordRef = useRef();
+  const repeatPasswordRef = useRef();
+
+  const changePassword = () => {
+    navigation.goBack();
+  };
 
   return (
-    <SafeAreaView style={styles.pageStyle}>
-      <AppBar label='Change Password' />
-      <View style={styles.contentStyle}>
-        <Text style={styles.textStyle}>Change Password</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Appbar.Header style={styles.AppbarHeader}>
+        <Appbar.BackAction
+          onPress={() => {
+            navigation.goBack();
+          }}
+          color={theme.colors.silverMetallic}
+        />
+        <Appbar.Content title='Change password' titleStyle={styles.AppbarTitleStyle} />
+      </Appbar.Header>
+      <ScrollView style={styles.SVcontainer}>
+        <InputField
+          label='Old password'
+          secure={true}
+          textInputProps={{
+            onChangeText: setNewPassword,
+            value: newPassword,
+            returnKeyType: 'next',
+            placeholder: 'Enter your old password',
+            onSubmitEditing: () => newPasswordRef?.current?.focus(),
+          }}
+        />
+        <View style={styles.separator16} />
+        <InputField
+          label='New password'
+          secure={true}
+          textInputProps={{
+            onChangeText: setOldPassword,
+            value: oldPassword,
+            returnKeyType: 'next',
+            placeholder: 'Enter your new password',
+            onSubmitEditing: () => repeatPasswordRef?.current?.focus(),
+            ref: newPasswordRef,
+          }}
+        />
+        <View style={styles.separator16} />
+        <InputField
+          label='Confirm new password'
+          secure={true}
+          textInputProps={{
+            onChangeText: setRepeatPassword,
+            value: repeatPassword,
+            returnKeyType: 'done',
+            placeholder: 'Confirm your new password',
+            ref: repeatPasswordRef,
+          }}
+        />
+        <View style={styles.separator40} />
+        <Button label='Submit' variant='contained' onPress={changePassword} />
+      </ScrollView>
+    </View>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  pageStyle: {
-    flexDirection: 'column',
+  container: {
     flex: 1,
     backgroundColor: theme.colors.background,
   },
-  contentStyle: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  AppbarHeader: {
+    elevation: 0,
+    backgroundColor: 'transparent',
   },
-  textStyle: {
+  AppbarTitleStyle: {
     color: theme.colors.text,
+    textTransform: 'capitalize',
+  },
+  SVcontainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
+  separator16: {
+    marginVertical: 8,
+  },
+  separator40: {
+    marginVertical: 20,
   },
 }));
 
