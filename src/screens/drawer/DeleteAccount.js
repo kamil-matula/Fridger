00dfa6df4c8d0059bb66fb3677efdd32f87ea0a6
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-10-16 15:43:35 +0200

    Add screen to delete account

--- src/screens/drawer/DeleteAccount.js
+++ src/screens/drawer/DeleteAccount.js
@@ -1,37 +1,88 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, View } from 'react-native';
+import { Appbar, useTheme } from 'react-native-paper';
 
-import { makeStyles } from '../../utils';
-import { AppBar } from '../../components';
+import { InputField, Button } from '../../components';
+import { makeStyles } from '../../utils/makeStyles';
 
-const DeleteAccount = () => {
+const DeleteAccount = ({ navigation }) => {
   const styles = useStyles();
+  const theme = useTheme();
+
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  const repeatPasswordRef = useRef();
+
+  const deleteAccount = () => {
+    navigation.goBack();
+  };
 
   return (
-    <SafeAreaView style={styles.pageStyle}>
-      <AppBar label='Delete Account' />
-      <View style={styles.contentStyle}>
-        <Text style={styles.textStyle}>Delete Account</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Appbar.Header style={styles.AppbarHeader}>
+        <Appbar.BackAction
+          onPress={() => {
+            navigation.goBack();
+          }}
+          color={theme.colors.silverMetallic}
+        />
+        <Appbar.Content title='Delete account' titleStyle={styles.AppbarTitleStyle} />
+      </Appbar.Header>
+      <ScrollView style={styles.SVcontainer}>
+        <InputField
+          label='Password'
+          secure={true}
+          textInputProps={{
+            onChangeText: setPassword,
+            value: password,
+            returnKeyType: 'next',
+            placeholder: 'Enter your password',
+            onSubmitEditing: () => repeatPasswordRef?.current?.focus(),
+          }}
+        />
+        <View style={styles.separator16} />
+        <InputField
+          label='Confirm password'
+          secure={true}
+          textInputProps={{
+            onChangeText: setRepeatPassword,
+            value: repeatPassword,
+            returnKeyType: 'done',
+            placeholder: 'Confirm your password',
+            ref: repeatPasswordRef,
+          }}
+        />
+        <View style={styles.separator40} />
+        <Button label='delete account' variant='containedRed' onPress={deleteAccount} />
+      </ScrollView>
+    </View>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  pageStyle: {
-    flexDirection: 'column',
+  container: {
     flex: 1,
     backgroundColor: theme.colors.background,
   },
-  contentStyle: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  AppbarHeader: {
+    elevation: 0,
+    backgroundColor: 'transparent',
   },
-  textStyle: {
+  AppbarTitleStyle: {
     color: theme.colors.text,
+    textTransform: 'capitalize',
+  },
+  SVcontainer: {
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
+  separator16: {
+    marginVertical: 8,
+  },
+  separator40: {
+    marginVertical: 20,
   },
 }));
 
