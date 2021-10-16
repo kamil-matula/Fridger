ed25e4591aae45280dcb9990a83ffc917eb057ec
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-10-16 19:03:58 +0200

    Use AppBar component

--- src/screens/drawer/ChangePassword.js
+++ src/screens/drawer/ChangePassword.js
@@ -1,14 +1,12 @@
 import React, { useState, useRef } from 'react';
 
 import { ScrollView, View } from 'react-native';
-import { Appbar, useTheme } from 'react-native-paper';
 
-import { InputField, Button } from '../../components';
+import { InputField, Button, AppBar } from '../../components';
 import { makeStyles } from '../../utils/makeStyles';
 
 const ChangePassword = ({ navigation }) => {
   const styles = useStyles();
-  const theme = useTheme();
 
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
@@ -23,15 +21,7 @@ const ChangePassword = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Appbar.Header style={styles.AppbarHeader}>
-        <Appbar.BackAction
-          onPress={() => {
-            navigation.goBack();
-          }}
-          color={theme.colors.silverMetallic}
-        />
-        <Appbar.Content title='Change password' titleStyle={styles.AppbarTitleStyle} />
-      </Appbar.Header>
+      <AppBar label='change password' />
       <ScrollView style={styles.SVcontainer}>
         <InputField
           label='Old password'
