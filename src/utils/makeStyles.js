35da78e08635635b8a69c40c3d2b473bb5033e04
Jan Rubel <jasiekrubel@gmail.com>
2021-10-17 16:56:04 +0200

    add jsdoc for makeStyles

--- src/utils/makeStyles.js
+++ src/utils/makeStyles.js
@@ -1,6 +1,29 @@
 import { StyleSheet } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
+/**
+ * @template T
+ * @extends StyleSheet.NamedStyles<T>
+ * @callback styles<T>
+ * @param {ReactNativePaper.Theme} [theme]
+ * @param {object} [props]
+ * @returns {T}
+ */
+
+/**
+ * @template T
+ * @extends StyleSheet.NamedStyles<T>
+ * @callback useStyles<T>
+ * @param {object} [props]
+ * @returns {T}
+ */
+
+/**
+ * @template T
+ * @extends StyleSheet.NamedStyles<T>
+ * @param {styles<T>} styles
+ * @returns {useStyles<T>}
+ */
 export const makeStyles =
   (styles) =>
   (props = null) => {
