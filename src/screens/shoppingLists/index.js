197d99b0d843f10ca40d5cf32220b8dfb92ddf76
kamil-matula <43967269+kamil-matula@users.noreply.github.com>
2021-11-21 20:41:59 +0100

    Remove Shopping List Scanner page

--- src/screens/shoppingLists/index.js
+++ src/screens/shoppingLists/index.js
@@ -2,6 +2,5 @@ export { default as AddShoppingList } from './AddShoppingList';
 export { default as AddShoppingListProduct } from './AddShoppingListProduct';
 export { default as ChooseFridge } from './ChooseFridge';
 export { default as ShoppingListDetails } from './ShoppingListDetails';
-export { default as ShoppingListScanner } from './ShoppingListScanner';
 export { default as ShoppingListsActive } from './ShoppingListsActive';
 export { default as ShoppingListsHistory } from './ShoppingListsHistory';
