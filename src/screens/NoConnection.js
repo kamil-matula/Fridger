5193788f95054bd99db748e7a1f504c55b861858
kamil-matula <43967269+kamil-matula@users.noreply.github.com>
2021-12-10 22:33:12 +0100

    Decrease package version to get rid of incompability warning

--- src/screens/NoConnection.js
+++ src/screens/NoConnection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BackHandler, View, StyleSheet } from 'react-native';
 import { Text, useTheme } from 'react-native-paper';
 
