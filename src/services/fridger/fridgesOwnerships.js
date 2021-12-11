f337f0f3b8a8a33c665653d4fc989ce2c839211c
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2021-12-11 09:45:50 +0100

    Add fridges ownerships queries

---
+++ src/services/fridger/fridgesOwnerships.js
@@ -0,0 +1,54 @@
+import { fridgerApi } from './fridgerApi';
+
+const fridgesOwnershipsApi = fridgerApi.injectEndpoints({
+  endpoints: (builder) => ({
+    fridgeOwners: builder.query({
+      query: (id) => ({
+        url: `fridges/${id}/ownerships`,
+        method: 'GET',
+      }),
+      providesTags: ['Fridges'],
+    }),
+    updatePermission: builder.mutation({
+      query: ({ userId, fridgeId, permissionName }) => ({
+        url: 'fridges-ownerships',
+        method: 'POST',
+        body: {
+          user: userId,
+          fridge: fridgeId,
+          permission: permissionName,
+        },
+      }),
+      invalidatesTags: ['Fridges'],
+    }),
+    addUser: builder.mutation({
+      query: ({ userId, fridgeId, permissionName }) => ({
+        url: `fridges-ownerships/${id}`,
+        method: 'PUT',
+        body: {
+          user: userId,
+          fridge: fridgeId,
+          permission: permissionName,
+        },
+      }),
+    }),
+    removeUser: builder.mutation({
+      query: ({ userId, fridgeId, permissionName }) => ({
+        url: `fridges/${id}`,
+        method: 'DELETE',
+        body: {
+          user: userId,
+          fridge: fridgeId,
+          permission: permissionName,
+        },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useFridgeOwnersQuery,
+  useUpdatePermissionMutation,
+  useAddUserMutation,
+  useRemoveUserMutation,
+} = fridgesOwnershipsApi;
