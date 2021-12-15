6be0aaf0c470d9a9439472a73325980a6e34b1f5
kamil-matula <43967269+kamil-matula@users.noreply.github.com>
2021-12-15 10:54:30 +0100

    Add remaining fridgeProduct endpoints

--- src/services/fridger/fridgeProducts.js
+++ src/services/fridger/fridgeProducts.js
@@ -1,14 +1,16 @@
 import { fridgerApi } from './fridgerApi';
 
+// TODO: Add producer to 'addFridgeProduct' and 'editFridgeProduct',
+// TODO: Remove 'SpecificFridge' tag after implementing GET
 const fridgeProductsApi = fridgerApi.injectEndpoints({
   endpoints: (builder) => ({
-    // fridgeProducts: builder.query({
-    //   query: () => ({
-    //     url: 'fridges-products',
-    //     method: 'GET',
-    //   }),
-    //   providesTags: ['FridgeProducts'],
-    // }),
+    fridgeProducts: builder.query({
+      query: () => ({
+        url: 'fridges-products',
+        method: 'GET',
+      }),
+      providesTags: ['FridgeProducts'],
+    }),
     addFridgeProduct: builder.mutation({
       query: ({
         name,
@@ -34,13 +36,38 @@ const fridgeProductsApi = fridgerApi.injectEndpoints({
           },
         },
       }),
-      // TODO: Remove 'SpecificFridge' after implementing GET
       invalidatesTags: ['FridgeProducts', 'SpecificFridge'],
+    }),
+    editFridgeProduct: builder.mutation({
+      query: ({ id, name, expiration }) => ({
+        url: `fridge-products/${id}`,
+        method: 'PATCH',
+        body: { name, expiration_date: expiration },
+      }),
+      invalidatesTags: ['Fridges', 'SpecificFridge'],
+    }),
+    deleteFridgeProduct: builder.mutation({
+      query: (id) => ({
+        url: `fridge-products/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Fridges', 'SpecificFridge'],
+    }),
+    updateFridgeProductQuantity: builder.mutation({
+      query: ({ product, status, quantity }) => ({
+        url: 'fridges-history-products',
+        method: 'POST',
+        body: { product, status, quantity },
+      }),
+      invalidatesTags: ['Fridges', 'SpecificFridge'],
     }),
   }),
 });
 
 export const {
-  // useFridgeProdcutsQuery,
+  useFridgeProductsQuery,
   useAddFridgeProductMutation,
+  useEditFridgeProductMutation,
+  useDeleteFridgeProductMutation,
+  useUpdateFridgeProductQuantityMutation,
 } = fridgeProductsApi;
